53d0320edfa95b4686cd4844ebc52aea7e247c92
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-11 00:25:55 +0530

    Added Login Form and Validations with Success and Failure Redirections

--- app.py
+++ app.py
@@ -46,9 +46,15 @@ def register():
         return redirect(url_for('home'))
     return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'Admin@123':
+            flash(f'Welcome, {form.email.data}! You have been logged in successfully!','success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessful. Please check email and password!','danger')
     return render_template("login.html", title="Login", form=form)
 
 if __name__=="__main__":

191d2d7cf23b20a4a92c0af45a0df8744c5de480
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-11 23:29:41 +0530

    Added SQLAlchemy for Database. Added User and Post Models

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ Created on Sat Aug  8 01:27:05 2020
 @author: harshit-saraswat
 """
 
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from secrets import Secrets
 from forms import RegistrationForm,LoginForm
 
@@ -13,6 +15,29 @@ from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
 secretsObj=Secrets()
 app.config['SECRET_KEY']=secretsObj.SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///site.db"
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(20), unique=True, nullable=False)
+    email=db.Column(db.String(120), unique=True, nullable=False)
+    image_file=db.Column(db.String(20), nullable=False, default="default.jpg")
+    password=db.Column(db.String(60), nullable=False)
+    posts=db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100), nullable=False)
+    date_posted=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content=db.Column(db.Text, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 posts = [
         {

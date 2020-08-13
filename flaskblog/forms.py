6a7c7813a158dc5c77d55101a8b2166779ccae95
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-13 23:26:30 +0530

    Added Registration DB Validations

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -7,7 +7,8 @@ Created on Mon Aug 10 23:08:25 2020
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired,Length,Email, EqualTo
+from wtforms.validators import DataRequired,Length,Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
@@ -15,6 +16,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
     submit=SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username taken. Please choose a different username.')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email taken. Please choose a different email.')
     
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])

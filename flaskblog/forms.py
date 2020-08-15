22561b8b8fd1a82188ef1792038a49b5ad656235
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-16 00:26:40 +0530

    Added Profile Picture Update functionality, finished accounts page section

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -6,6 +6,7 @@ Created on Mon Aug 10 23:08:25 2020
 """
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired,Length,Email, EqualTo, ValidationError
@@ -37,6 +38,7 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
     email = StringField('Email',validators=[DataRequired(),Email()])
+    picture=FileField('Update Profile Picture', validators=[FileAllowed(['png','jpg','jpeg'])])
     submit=SubmitField('Update')
 
     def validate_username(self,username):
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -5,6 +5,9 @@ Created on Wed Aug  12 11:49:05 2020
 @author: harshit-saraswat
 """
 
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm,LoginForm, UpdateAccountForm
@@ -70,12 +73,25 @@ def logout():
     flash('Logged out successfully!','info')
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _,ext=os.path.splitext(form_picture.filename)
+    picture_fn=random_hex+ext
+    picture_path=os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    op_size=(125,125)
+    i=Image.open((form_picture))
+    i.thumbnail(op_size)
+    i.save(picture_path)
+    return picture_fn
 
 @app.route("/account",methods=['GET','POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file=picture_file
         current_user.username=form.username.data
         current_user.email=form.email.data
         db.session.commit()

4ec8006dec2af52da8a112661e1509fe2f2262c5
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-16 00:01:15 +0530

    Added Account Details Form

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -6,6 +6,7 @@ Created on Mon Aug 10 23:08:25 2020
 """
 
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired,Length,Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -32,3 +33,20 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit=SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    submit=SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username taken. Please choose a different username.')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email taken. Please choose a different email.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -7,7 +7,7 @@ Created on Wed Aug  12 11:49:05 2020
 
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm,LoginForm
+from flaskblog.forms import RegistrationForm,LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -71,8 +71,18 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account",methods=['GET','POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method=='GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email
     image_file= url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template("account.html", title="Account", image_file=image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form = form)

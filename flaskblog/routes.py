466ea8124e0b42a4294d9493b0055e9c8aae9d50
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-17 23:36:18 +0530

    Added View Posts for Specific Users and Post Ordering

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -137,7 +137,7 @@ def delete_post(post_id):
     return redirect(url_for('home'))
 
 @app.route("/user/<string:username>")
-def user_posts(username)
+def user_posts(username):
     page=request.args.get('page',1,type=int)
     user=User.query.filter_by(username=username).first_or_404()
     posts=Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)

157fa47d4d31148defbfdf0e4acc4d44d231ba0a
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-20 23:59:28 +0530

    Started with Flask Blueprints

---
+++ flaskblog/users/utils.py
@@ -0,0 +1,22 @@
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _,ext=os.path.splitext(form_picture.filename)
+    picture_fn=random_hex+ext
+    picture_path=os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    op_size=(125,125)
+    i=Image.open((form_picture))
+    i.thumbnail(op_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+def send_reset_email(user):
+    token=user.get_reset_token()
+    msg=Message('Password Reset Request', sender='saraswat2204@gmail.com', recipients=[user.email])
+    msg.body=f"""
+    To reset your password, visit the following link:
+    {url_for('reset_token',token=token,_external=True)}
+
+    If you did not make this request then simply ignore this email and no changes will be made.
+    """
+    mail.send(msg)

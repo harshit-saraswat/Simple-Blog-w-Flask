eef29349210bdc4e5f28b7057df03705a419e00e
harshit-saraswat <contactharshitsaraswat@gmail.com>
2020-08-24 22:51:57 +0530

    Added Login Required Conditions for various pages

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post
+from flask_login import current_user
 
 main = Blueprint('main',__name__)
 
@@ -8,7 +9,7 @@ main = Blueprint('main',__name__)
 def home():
     page=request.args.get('page',1,type=int)
     posts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template("home.html",posts=posts)
+    return render_template("home.html",posts=posts, current_user=current_user)
 
 @main.route("/about")
 def about():
